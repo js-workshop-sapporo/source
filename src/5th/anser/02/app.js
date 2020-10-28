70d86cc10111c8a2d56aa818ad77425d5a192562
redamoon <yuuki60824@gmail.com>
2020-10-28 12:02:25 +0900

    add Accordionの実装

--- src/5th/anser/02/app.js
+++ src/5th/anser/02/app.js
@@ -1,15 +1,29 @@
 (function(window, document){
-  const button = document.getElementById('accordion__trigger');
-  const body = document.getElementById('toggle__body');
-  const clickToggle = (_clientH) => {
-    const lastH = body.style.height;
-    body.style.height = (lastH == '' || lastH == '0px') ? _clientH + 'px' : '0px';
+  // クリック要素のセレクターを取得する
+  const accordionTrigger = document.querySelectorAll('.card-header');
+  /**
+   * init 初期値はターゲットは開いた状態にする
+   * @param target
+   */
+  const init = (target) => {
+    target.style.height = target.scrollHeight + 'px';
+    target.classList.add('show');
   };
-  const init = () => {
-    const _clientH = body.clientHeight;
-    body.style.height = 'auto';
-    body.style.height = '0px';
-    button.addEventListener('click', () => clickToggle(_clientH), false);
-  };
-  init();
+  [...accordionTrigger].forEach((value, index) => {
+    // 初期値は一個目のターゲットは開く
+    init(accordionTrigger[0].nextElementSibling);
+    // クリックイベント
+    accordionTrigger[index].addEventListener('click', (e) => {
+      const currentElement = e.currentTarget;
+      const accordionTarget = currentElement.nextElementSibling;
+      const currentClass = accordionTarget.classList.contains('show');
+      if (!currentClass) {
+        accordionTarget.style.height = accordionTarget.scrollHeight + 'px';
+        accordionTarget.classList.add('show');
+      } else {
+        accordionTarget.style.height = null;
+        accordionTarget.classList.remove('show');
+      }
+    });
+  });
 })(window,document);
