4abe66aa6201d531174b55a080d36e4744eb1140
redamoon <yuuki60824@gmail.com>
2020-11-11 05:01:03 +0900

    fix ドラッグ処理

--- src/5th/answer/06/app.js
+++ src/5th/answer/06/app.js
@@ -1,59 +1,49 @@
 (function(window, document){
-// ドラッグ&ドロップエリアの取得
-  const fileArea = document.getElementById('dropArea');
+  const fileArea = document.getElementById('dragDropArea');
+  const fileInput = document.getElementById('fileInput');
 
-// input[type=file]の取得
-  const fileInput = document.getElementById('uploadFile');
+  /**
+   * onChangePreview
+   */
+  const onChangePreview = (event, f = null) => {
+    let file = f;
+    if(file === null){
+      file = event.target.files[0];
+    }
+    let reader = new FileReader();
+    const preview = document.getElementById('previewArea');
+    const previewImage = document.getElementById('previewImage');
 
-  const filePrint = document.getElementById('printArea');
+    if(previewImage != null) {
+      preview.removeChild(previewImage);
+    }
 
-// ドラッグオーバー時の処理
-  fileArea.addEventListener('dragover', function(e){
-    e.preventDefault();
+    reader.onload = function(event) {
+      const img = document.createElement('img');
+      img.setAttribute('src', reader.result);
+      img.setAttribute('id', 'previewImage');
+      preview.appendChild(img);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  fileArea.addEventListener('dragover', function(evt){
+    evt.preventDefault();
     fileArea.classList.add('dragover');
   });
-
-// ドラッグアウト時の処理
-  fileArea.addEventListener('dragleave', function(e){
-    e.preventDefault();
+  fileArea.addEventListener('dragleave', function(evt){
+    evt.preventDefault();
     fileArea.classList.remove('dragover');
   });
-
-// ドロップ時の処理
-  fileArea.addEventListener('drop', function(e){
-    e.preventDefault();
-    fileArea.classList.remove('dragover');
-
-    // ドロップしたファイルの取得
-    const files = e.dataTransfer.files;
-
-    // 取得したファイルをinput[type=file]へ
+  fileArea.addEventListener('drop', function(evt){
+    evt.preventDefault();
+    fileArea.classList.remove('dragenter');
+    const files = evt.dataTransfer.files;
+    console.log("DRAG & DROP");
+    console.table(files);
     fileInput.files = files;
-
-    if(typeof files[0] !== 'undefined') {
-      //ファイルが正常に受け取れた際の処理
-      const img = document.createElement("img");
-      img.src = files;
-      filePrint.appendChild(img);
-      console.log('aaaa')
-    } else {
-      //ファイルが受け取れなかった際の処理
-    }
+    onChangePreview('onChange',files[0]);
   });
 
-// input[type=file]に変更があれば実行
-// もちろんドロップ以外でも発火します
-  fileInput.addEventListener('change', function(e){
-    const file = e.target.files[0];
-
-    console.log('チェンジ')
-    if(typeof e.target.files[0] !== 'undefined') {
-      // ファイルが正常に受け取れた際の処理
-      const img = document.createElement("img");
-      img.src = e.target.files[0];
-      filePrint.appendChild(img);
-    } else {
-      // ファイルが受け取れなかった際の処理
-    }
-  }, false);
 })(window,document);
